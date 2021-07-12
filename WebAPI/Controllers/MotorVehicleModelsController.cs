a863c746a38720b43395602c08d27f48d1d02fc1
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-07-12 18:23:47 +0800

    Commit 2021-07-12-1822 Change DomainModel project Target Framework Version to 4.7.2 Change DomainModelTest project Target Framework Version to 4.7.2 Add WebAPI with 2 ODataControllers

---
+++ WebAPI/Controllers/MotorVehicleModelsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.OData;
+using DomainModel;
+using Ent = DomainModel.Entities;
+
+namespace WebAPI.Controllers
+{
+    public class MotorVehicleModelsController : ODataController
+    {
+        VehicleRentalContext context = new VehicleRentalContext();
+
+        private bool MotorVehicleModelExists(Guid key)
+        {
+            return context.MotorVehicleModels.Any(mvm => mvm.MotorVehicleModelId == key);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<Ent.MotorVehicleModel> Get()
+        {
+            return context.MotorVehicleModels;
+        }
+
+        [EnableQuery]
+        public SingleResult<Ent.MotorVehicleModel> Get([FromODataUri] Guid key)
+        {
+            IQueryable<Ent.MotorVehicleModel> result = context.MotorVehicleModels.Where(mvm => mvm.MotorVehicleModelId == key);
+            return SingleResult.Create(result);
+        }
+
+        [EnableQuery]
+        public IQueryable<Ent.MotorVehicle> GetMotorVehicles([FromODataUri] Guid key)
+        {
+            var vehicleModel = context.MotorVehicleModels.FirstOrDefault(mvm => mvm.MotorVehicleModelId == key);
+            if (vehicleModel == null)
+                return null;
+            return vehicleModel.MotorVehicles.AsQueryable();
+        }
+    }
+}
---
+++ WebAPI/Controllers/VehicleMakesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.OData;
+using DomainModel;
+using Ent = DomainModel.Entities;
+
+namespace WebAPI.Controllers
+{
+    public class VehicleMakesController : ODataController
+    {
+        VehicleRentalContext context = new VehicleRentalContext();
+
+        private bool VehicleMakeExists(Guid key)
+        {
+            return context.VehicleMakes.Any(vm => vm.VehicleMakeId == key);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<Ent.VehicleMake> Get()
+        {
+            return context.VehicleMakes;
+        }
+
+        [EnableQuery]
+        public SingleResult<Ent.VehicleMake> Get([FromODataUri] Guid key)
+        {
+            IQueryable<Ent.VehicleMake> result = context.VehicleMakes.Where(vm => vm.VehicleMakeId == key);
+            return SingleResult.Create(result);
+        }
+
+        [EnableQuery]
+        public IQueryable<Ent.MotorVehicleModel> GetMotorVehicleModels([FromODataUri] Guid key)
+        {
+            var vehicleMake = context.VehicleMakes.FirstOrDefault(vm => vm.VehicleMakeId == key);
+            if (vehicleMake == null)
+                return null;
+            return vehicleMake.MotorVehicleModels.AsQueryable();
+        }
+    }
+}
