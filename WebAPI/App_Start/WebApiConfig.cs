4dd6db48d4c86565442d22a17ea56d653974965f
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-07-16 17:33:20 +0800

    Commit 2021-07-16-1732 Add OData $batch support

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using ODat = Microsoft.AspNet.OData;
 using Ent = DomainModel.Entities;
+using Microsoft.AspNet.OData.Batch;
 
 namespace WebAPI
 {
@@ -31,14 +32,27 @@ namespace WebAPI
             //);
 
             config.OrderBy().Count().MaxTop(null).SkipToken().Filter().Expand();
+
             ODataModelBuilder builder = new ODataConventionModelBuilder();
             builder.EntitySet<Ent.VehicleMake>("VehicleMakes");
             builder.EntitySet<Ent.MotorVehicleModel>("MotorVehicleModels");
             builder.EntitySet<Ent.MotorVehicle>("MotorVehicles");
+
+            //Read the following for more information regarding $batch
+            //https://devblogs.microsoft.com/odata/all-in-one-with-odata-batch/
+            //https://docs.microsoft.com/en-us/odata/webapi/batch
+            //https://docs.microsoft.com/en-us/odata/client/batch-operations
+            //https://docs.oasis-open.org/odata/odata-json-format/v4.01/os/odata-json-format-v4.01-os.html#sec_BatchRequestsandResponses
+            var odataBatchHandler = new DefaultODataBatchHandler(GlobalConfiguration.DefaultServer);
+            odataBatchHandler.MessageQuotas.MaxNestingDepth = 2;
+            odataBatchHandler.MessageQuotas.MaxOperationsPerChangeset = 10;
+            odataBatchHandler.MessageQuotas.MaxReceivedMessageSize = 100;
+
             config.MapODataServiceRoute(
                 routeName: "ODataRoute",
                 routePrefix: null,
-                model: builder.GetEdmModel());
+                model: builder.GetEdmModel(),
+                batchHandler: odataBatchHandler);
         }
     }
 }
